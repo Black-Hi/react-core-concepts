7359e25a9fd07a29deb8a07b1d08f324cb3214c8
Black-Hi <diptojarnal@gmail.com>
2022-01-03 18:39:39 +0600

    moudy_full

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(10);
+  const handleIncrease = () => {
+    // const newCount = count + 1;
+    setCount(count + 1);
+  };
+  function Users() {
+    const [users, setUser] = useState([]);
+    useEffect(() => {
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then((res) => res.json())
+        .then((data) => setUser(data));
+    }, []);
+    return (
+      <div>
+        <h3>Dynamic User: {users.length}</h3>
+        {users.map((user) => (
+          <li>{user.name}</li>
+        ))}
+      </div>
+    );
+  }
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1>Count:{count}</h1>
+      <Users></Users>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={handleIncrease}>Increase</button>
     </div>
   );
 }
